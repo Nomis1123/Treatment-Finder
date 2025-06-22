bd9335097f4dc62b58548164853fa3be42009ae1
Nomis1123 <72155251+Nomis1123@users.noreply.github.com>
2025-06-22 14:28:42 -0400

    updated data sifting and gitignore

--- dataSifting.py
+++ dataSifting.py
@@ -7,6 +7,13 @@ import csv
 
 pd.set_option('display.max_columns', None)
 pd.set_option('display.width', 1000)
+
 file_path_patient = "./Data/patientData.csv"
 file_path_hospital = "./Data/hospitalData.csv"
+
+patient_df = pd.read_csv(file_path_patient)
+hospital_df = pd.read_csv(file_path_hospital)
+
+print(patient_df.info())
+
 
