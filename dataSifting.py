77ceb3fbba2a1ec7c845644ed2a84b94d6650e34
Nomis1123 <72155251+Nomis1123@users.noreply.github.com>
2025-06-22 14:04:02 -0400

    setting enviromental stuff up for data

---
+++ dataSifting.py
@@ -0,0 +1,12 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import statsmodels.api as sm
+import seaborn as sns
+import csv
+
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', 1000)
+file_path_patient = "./Data/patientData.csv"
+file_path_hospital = "./Data/hospitalData.csv"
+
